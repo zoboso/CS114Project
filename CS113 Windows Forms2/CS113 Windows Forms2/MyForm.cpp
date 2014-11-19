cd9e2a199dcf28d73d1142362cd01d3d6a91fb34
zoboso <joseph.m.cole343@gmail.com>
2014-11-19 13:56:13 -0500

    started to modify input code for entity use.

--- CS113 Windows Forms2/CS113 Windows Forms2/MyForm.cpp
+++ CS113 Windows Forms2/CS113 Windows Forms2/MyForm.cpp
@@ -1,4 +1,8 @@
 #include "MyForm.h"
+#include <vector>
+#include "C:\Users\Joseph\Documents\GitHub\CS114Project\Controller\Controller\Entity.h"
+#include "C:\Users\Joseph\Documents\GitHub\CS114Project\Controller\Controller\Entity.cpp"
+#include "Input.cpp"
 
 using namespace WinFormsProjectTemplate; //name of project
 
@@ -12,10 +16,13 @@ int main(array<System::String ^> ^args)
 	// Create the main window and run it
 	Application::Run(gcnew MyForm());
 	return 0;
+	
+	std::vector< std::vector< Entity >> map;
+	int level = 1;
 
 	{
 		//Level's loop
-
+		input(map, level++)
 
 	}
 }
